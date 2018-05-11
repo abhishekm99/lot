ff37ca8be82763ad0a7a5dafdbed986a6a56e03d
abhishek malakar <abhishekmalakar4@gmail.com>
2018-05-11 16:21:41 +0530

    Contact test

---
+++ mailscript.php
@@ -0,0 +1,14 @@
+<?php
+
+$from="noreply@gmail.com";
+$to="abhishekmalakar4@gmail.com";
+$name=$_POST['name'];
+$email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+mail ( $to, $name, $email, $subject, $message, "From:".$from);
+
+Print "Your message has been sent";
+
+?>
