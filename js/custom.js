0e823d382caf524e6a9bb2be5beaa45bdd4f8309
abhishek malakar <abhishekmalakar4@gmail.com>
2018-12-16 21:52:23 +0530

    New committees

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,35 @@ jQuery(document).ready(function() {
 	// Your custom js code goes here.
 
 });
+
+$(function() { 
+    var carousel = $(".carousel"),
+       currdeg  = 0;
+   $(".nexter").on("click", { d: "n" }, rotate);
+   $(".prever").on("click", { d: "p" }, rotate);
+   $(".next").on("click", { d: "n" }, rotate);
+   $(".prev").on("click", { d: "p" }, rotate);
+    
+   function rotate(e){
+     if(e.data.d=="n"){
+	   currdeg = currdeg - 72;
+     }
+     if(e.data.d=="p"){
+	   currdeg = currdeg + 72;
+     }
+     carousel.css({
+       "-webkit-transform": "rotateY("+currdeg+"deg)",
+       "-moz-transform": "rotateY("+currdeg+"deg)",
+       "-o-transform": "rotateY("+currdeg+"deg)",
+       "transform": "rotateY("+currdeg+"deg)"
+     });
+   }
+   });
+
+   function on() {
+			document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+			document.getElementById("overlay").style.display = "none";
+  }
